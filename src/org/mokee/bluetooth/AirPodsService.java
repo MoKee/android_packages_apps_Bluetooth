60497602fe9d474511b515a2116f5ab31ec927c1
XiNGRZ <chenxingyu92@gmail.com>
2019-12-09 14:26:53 +0800

    Bluetooth: Add additional support for Apple AirPods series
    Change-Id: I67c13072e54be3d98a3105aec8360bb5e0e1b3dc

---
+++ src/org/mokee/bluetooth/AirPodsService.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2019 The MoKee Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mokee.bluetooth;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class AirPodsService extends Service {
+
+    private static final String TAG = "AirPodsService";
+
+    public static Intent getIntent(Context context) {
+        return new Intent(context, AirPodsService.class);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.v(TAG, "onCreate");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "onDestroy");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.v(TAG, "onStartCommand");
+        return START_STICKY;
+    }
+
+}
